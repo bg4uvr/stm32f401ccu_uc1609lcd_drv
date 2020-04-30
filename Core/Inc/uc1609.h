c8dcd972b1b1f487f99322a8a41900904ac94384
bg4uvr <bg4uvr@qq.com>
2020-04-30 13:02:17 +0800

    重新选择了自动格式化代码的格式，主循环中加入了少量注释

--- Core/Inc/uc1609.h
+++ Core/Inc/uc1609.h
@@ -18,19 +18,18 @@
 //CD    PA6
 //SDA   PA7
 
-
 extern uint8_t disp_buf[];
 
 //声明功能函数
 void lcd_init(void);
 void lcd_cls(uint8_t rev);
-void display_one_char(uint8_t x,uint8_t y,char char_data,uint8_t rev);
-void display_list_char(uint8_t x,uint8_t y,char *data_s,uint8_t rev);
-void display_816_char(uint8_t x,uint8_t y,char char_data, uint8_t rev);
-void display_list_816(uint8_t x,uint8_t y,char *data_s, uint8_t rev);
+void display_one_char(uint8_t x, uint8_t y, char char_data, uint8_t rev);
+void display_list_char(uint8_t x, uint8_t y, char *data_s, uint8_t rev);
+void display_816_char(uint8_t x, uint8_t y, char char_data, uint8_t rev);
+void display_list_816(uint8_t x, uint8_t y, char *data_s, uint8_t rev);
 
-void gui_point(uint8_t X,uint8_t Y,uint8_t draw_mode);
-void glcd_line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t show) ;
+void gui_point(uint8_t X, uint8_t Y, uint8_t draw_mode);
+void glcd_line(int16_t x1, int16_t y1, int16_t x2, int16_t y2, uint8_t show);
 void glcd_circle(int16_t x, int16_t y, int16_t radius, uint8_t show);
 
 #endif /* INC_UC1609_H_ */
